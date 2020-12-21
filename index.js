3d6b5aedf22a91ff0c3e87564db4fd4af10e265a
Nicholas O'Hara <nicholas.ohara@gmail.com>
2020-12-21 08:15:08 -0500

    initial version

---
+++ index.js
@@ -0,0 +1,28 @@
+const robot = require('robotjs');
+
+robot.setMouseDelay(2);
+
+var twoPI = Math.PI * 2.0;
+var screenSize = robot.getScreenSize();
+var height = (screenSize.height / 2) -10;
+var width = screenSize.width;
+
+function doSetTimeout() {
+    setTimeout(function() {
+        for (var x=0; x < width; x++) {
+            y = height * Math.sin((twoPI * x) / width) + height;
+            robot.moveMouse(x,y);
+        }
+
+        robot.typeString("The quick brown fox jumped over the lazy dog.");
+        robot.keyTap("enter");
+        robot.keyToggle("shift", "down");
+        robot.keyTap("up");
+        robot.keyToggle("shift", "up");
+        robot.keyTap("backspace");
+
+        doSetTimeout();
+    }, 15000);
+}
+
+doSetTimeout();
